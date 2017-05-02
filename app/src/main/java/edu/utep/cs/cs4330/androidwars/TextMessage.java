b1701ccd164d47deb0887d212cb9a85fd435ab95
Jose Perez <josegperez@mail.com>
2017-05-01 18:41:02 -0600

    Fixed background coloring on messages

--- app/src/main/java/edu/utep/cs/cs4330/androidwars/TextMessage.java
+++ app/src/main/java/edu/utep/cs/cs4330/androidwars/TextMessage.java
@@ -3,10 +3,17 @@ package edu.utep.cs.cs4330.androidwars;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.text.TextPaint;
 import android.view.View;
 
 public class TextMessage {
+    /**
+     * Margin added to all messages with backgrounds
+     */
+    private static final float BG_MARGIN = 15;
+
     public String message;
     public TextPosition position;
     public float fontSize;
@@ -28,17 +35,18 @@ public class TextMessage {
     }
 
     public void draw(View view, Canvas canvas){
-        RectF rect = getRect(view);
-
+        // Background drawing
         Paint backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         backgroundPaint.setColor(backgroundColor);
+        canvas.drawRect(getBackgroundRect(view), backgroundPaint);
 
-        canvas.drawRect(rect, backgroundPaint);
+        // Text drawing
+        RectF rect = getRect(view);
         canvas.drawText(message, rect.left, rect.top, getPaint());
     }
 
     private Paint getPaint(){
-        Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+        Paint p = new TextPaint(Paint.ANTI_ALIAS_FLAG);;
         p.setTextSize(fontSize);
         p.setColor(fontColor);
         return p;
@@ -55,21 +63,36 @@ public class TextMessage {
             x = view.getMeasuredWidth() / 2;
             y = view.getTop() + getTextHeight();
         }
+        // Adjust alignment based on text width
+        // Otherwise it isn't properly centered
+        x -= getTextWidth() / 2;
 
-        x -= getTextWidth();
+        return new RectF(x, y , 0, 0);
+    }
 
-        float width = getTextWidth();
-        float height = getTextHeight();
+    public Rect getBackgroundRect(View view){
+        // Get the rectangle that can fit the text
+        Rect rectBackground = new Rect();
+        getPaint().getTextBounds(message, 0, message.length(), rectBackground);
+        rectBackground.left -= BG_MARGIN;
+        rectBackground.right += BG_MARGIN;
+        rectBackground.top -= BG_MARGIN;
+        rectBackground.bottom += BG_MARGIN;
 
-        return new RectF(x, y, width, height);
+        // Place it where the text actually is
+        RectF rectText = getRect(view);
+        rectBackground.offset((int)rectText.left, (int)rectText.top);
+
+        return rectBackground;
     }
 
     private float getTextWidth() {
-        return getPaint().measureText(message) / 2;
+        return getPaint().measureText(message);
     }
 
     private float getTextHeight(){
-        return getPaint().getTextSize();
+        Paint.FontMetrics fm = getPaint().getFontMetrics();
+        return fm.descent - fm.ascent;
     }
 
     @Override
